96610920517d369a3daa9668a5ca84957841d27b
Ahira Adefokun <ahira.adefokun@teamapt.com>
2021-08-11 16:06:18 +0100

    Implement UserDetailsService interface on authservice

--- src/main/java/com/ahirajustice/app/ws/services/auth/AuthService.java
+++ src/main/java/com/ahirajustice/app/ws/services/auth/AuthService.java
@@ -1,14 +1,31 @@
 package com.ahirajustice.app.ws.services.auth;
 
+import java.util.ArrayList;
+
+import com.ahirajustice.app.ws.entities.User;
+import com.ahirajustice.app.ws.repositories.IUserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class AuthService implements IAuthService{
+@Service
+public class AuthService implements IAuthService {
+
+    @Autowired
+    IUserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getEncryptedPassword(),
+                new ArrayList<>());
     }
-    
+
 }
