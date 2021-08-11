95a7e278e7af766e0f3646d42d195215e257d1b8
Ahira Adefokun <ahira.adefokun@teamapt.com>
2021-08-11 13:32:31 +0100

    Create user entity

---
+++ src/main/java/com/ahirajustice/app/ws/entities/User.java
@@ -0,0 +1,92 @@
+package com.ahirajustice.app.ws.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "users")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 5865903736190450273L;
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    private String emailVerificationToken;
+
+    @Column(nullable = false)
+    private Boolean emailVerificationStatus = false;
+
+    @Column(nullable = false)
+    private String encryptedPassword;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmailVerificationToken() {
+        return emailVerificationToken;
+    }
+
+    public void setEmailVerificationToken(String emailVerificationToken) {
+        this.emailVerificationToken = emailVerificationToken;
+    }
+
+    public Boolean getEmailVerificationStatus() {
+        return emailVerificationStatus;
+    }
+
+    public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
+        this.emailVerificationStatus = emailVerificationStatus;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
