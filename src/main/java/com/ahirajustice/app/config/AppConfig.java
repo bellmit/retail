5380e97a64a96877a0af8678f18b56ad7b038059
Ahira Adefokun <ahira.adefokun@teamapt.com>
2021-09-05 09:18:22 +0100

    Add config for superadmin to AppConfig

--- src/main/java/com/ahirajustice/app/config/AppConfig.java
+++ src/main/java/com/ahirajustice/app/config/AppConfig.java
@@ -5,9 +5,23 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AppConfig {
+
     @Value("${app.config.accesstokenexpireminutes}")
     public int ACCESS_TOKEN_EXPIRE_MINUTES;
 
     @Value("${app.config.secretkey}")
     public String SECRET_KEY;
+
+    @Value("${app.config.superuser.email}")
+    public String SUPERUSER_EMAIL;
+
+    @Value("${app.config.superuser.firstname}")
+    public String SUPERUSER_FIRST_NAME;
+
+    @Value("${app.config.superuser.lastname}")
+    public String SUPERUSER_LAST_NAME;
+
+    @Value("${app.config.superuser.password}")
+    public String SUPERUSER_PASSWORD;
+
 }
