9b374279c957a7755dd1edceda7ebcbbd3b79831
Ahira Adefokun <justiceahira@gmail.com>
2021-09-27 13:14:31 +0100

    Exclude openapi and swagger urls for auth

--- src/main/java/com/ahirajustice/app/constants/SecurityConstants.java
+++ src/main/java/com/ahirajustice/app/constants/SecurityConstants.java
@@ -7,6 +7,12 @@ public class SecurityConstants {
     public static final String HEADER_STRING = "Authorization";
 
     // URLs
-    public static final String[] EXCLUDE_FROM_AUTH_URLS = new String[] { "/api/auth/login, POST", "/api/users, POST" };
+    public static final String[] EXCLUDE_FROM_AUTH_URLS = new String[] { 
+        "/api/auth/login, POST", 
+        "/api/users, POST",
+        "/api/retail/docs, GET",
+        "/api/retail/docs.yaml, GET",
+        "/api/retail/**, GET"
+    };
 
 }
